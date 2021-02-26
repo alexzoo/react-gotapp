efbdf5f30027f4d8f32f025941f72c1adaea6521
a.zubov <a.zubov@yadro.com>
2021-02-26 18:01:20 +0300

    task 97

--- src/components/charDetails/charDetails.js
+++ src/components/charDetails/charDetails.js
@@ -1,30 +1,46 @@
-import React, {Component} from 'react';
-import './charDetails.css';
-export default class CharDetails extends Component {
+import React, { Component } from 'react'
+import styled from 'styled-components'
 
-    render() {
-        return (
-            <div className="char-details rounded">
-                <h4>John Snow</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Gender</span>
-                        <span>male</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Born</span>
-                        <span>1783</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Died</span>
-                        <span>1820</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Culture</span>
-                        <span>First</span>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+const CharDetailsDiv = styled.div`
+	background-color: #fff;
+	padding: 25px 25px 15px 25px;
+	margin-bottom: 40px;
+`
+
+const CharTitle = styled.h4`
+	margin-bottom: 20px;
+	text-align: center;
+`
+
+// const SelectError = styled.div`
+//     color: #fff;
+//     text-align: center;
+//     font-size: 26px;
+// `
+export default class CharDetails extends Component {
+	render() {
+		return (
+			<CharDetailsDiv className='rounded'>
+				<CharTitle>John Snow</CharTitle>
+				<ul className='list-group list-group-flush'>
+					<li className='list-group-item d-flex justify-content-between'>
+						<span className='term'>Gender</span>
+						<span>male</span>
+					</li>
+					<li className='list-group-item d-flex justify-content-between'>
+						<span className='term'>Born</span>
+						<span>1783</span>
+					</li>
+					<li className='list-group-item d-flex justify-content-between'>
+						<span className='term'>Died</span>
+						<span>1820</span>
+					</li>
+					<li className='list-group-item d-flex justify-content-between'>
+						<span className='term'>Culture</span>
+						<span>First</span>
+					</li>
+				</ul>
+			</CharDetailsDiv>
+		)
+	}
 }
--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,32 +1,69 @@
-import React, {Component} from 'react';
-import './randomChar.css';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import gotService from '../../services/gotService'
+
+const RandomBlock = styled.div`
+	background-color: #fff;
+	padding: 25px 25px 15px 25px;
+	margin-bottom: 40px;
+`
+
+const RandomTitle = styled.div`
+	margin-bottom: 20px;
+	text-align: center;
+`
+
+const TermSpan = styled.span`
+	font-weight: bold;
+`
 
 export default class RandomChar extends Component {
+	constructor() {
+		super()
+		this.updateChar()
+	}
 
-    render() {
+	gotService = new gotService()
 
-        return (
-            <div className="random-block rounded">
-                <h4>Random Character: John</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Gender </span>
-                        <span>male</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Born </span>
-                        <span>11.03.1039</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Died </span>
-                        <span>13.09.1089</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Culture </span>
-                        <span>Anarchy</span>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+	state = {
+		char: {}
+	}
+
+	onCharLoaded = (char) => {
+		this.setState({ char })
+	}
+
+	updateChar() {
+		const id = Math.floor(Math.random() * 140 + 25) // 25 - 140
+		this.gotService.getCharacter(id).then(this.onCharLoaded)
+	}
+
+	render() {
+		const {
+			char: { name, gender, born, died, culture }
+		} = this.state
+		return (
+			<RandomBlock className='rounded'>
+				<RandomTitle>Random Character: {name}</RandomTitle>
+				<ul className='list-group list-group-flush'>
+					<li className='list-group-item d-flex justify-content-between'>
+						<TermSpan>Gender</TermSpan>
+						<span>{gender}</span>
+					</li>
+					<li className='list-group-item d-flex justify-content-between'>
+						<TermSpan>Born </TermSpan>
+						<span>{born}</span>
+					</li>
+					<li className='list-group-item d-flex justify-content-between'>
+						<TermSpan>Died </TermSpan>
+						<span>{died}</span>
+					</li>
+					<li className='list-group-item d-flex justify-content-between'>
+						<TermSpan>Culture </TermSpan>
+						<span>{culture}</span>
+					</li>
+				</ul>
+			</RandomBlock>
+		)
+	}
 }
